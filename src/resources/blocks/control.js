818924b21087c8952ae6d4ab49722d2ac465eaf5
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-08 16:10:40 +0000

    switch n break

--- src/resources/blocks/control.js
+++ src/resources/blocks/control.js
@@ -32,6 +32,7 @@ function register() {
         const code = `if (${CONDITION ? `Boolean(${CONDITION})` : 'false'}) { ${BLOCKS} };`;
         return `${code}\n`;
     })
+
     // if <> then {} else {}
     registerBlock(`${categoryPrefix}ifthenelse`, {
         message0: 'if %1 then %2 %3 else %4 %5',
@@ -69,6 +70,94 @@ function register() {
         } else {
             ${BLOCKS2}
         };`;
+        return `${code}\n`;
+    })
+
+    // switch statement
+    registerBlock(`${categoryPrefix}switch`, {
+        message0: 'switch %1 %2 %3',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "VALUE"
+            },
+            {
+                "type": "input_dummy"
+            },
+            {
+                "type": "input_statement",
+                "name": "BLOCKS",
+                "check": "Case"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const VALUE = javascriptGenerator.valueToCode(block, 'VALUE', javascriptGenerator.ORDER_ATOMIC);
+        const BLOCKS = javascriptGenerator.statementToCode(block, 'BLOCKS');
+        const code = `switch (${VALUE || "''"}) { ${BLOCKS} };`;
+        return `${code}\n`;
+    })
+
+    // case
+    registerBlock(`${categoryPrefix}case`, {
+        message0: 'case %1 %2 %3',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "VALUE"
+            },
+            {
+                "type": "input_dummy"
+            },
+            {
+                "type": "input_statement",
+                "name": "BLOCKS"
+            }
+        ],
+        previousStatement: "Case",
+        nextStatement: "Case",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const VALUE = javascriptGenerator.valueToCode(block, 'VALUE', javascriptGenerator.ORDER_ATOMIC);
+        const BLOCKS = javascriptGenerator.statementToCode(block, 'BLOCKS');
+        const code = `case (${VALUE || "''"}): ${BLOCKS}`;
+        return `${code}\n`;
+    })
+
+    // default
+    registerBlock(`${categoryPrefix}default`, {
+        message0: 'default %1 %2',
+        args0: [
+            {
+                "type": "input_dummy"
+            },
+            {
+                "type": "input_statement",
+                "name": "BLOCKS"
+            }
+        ],
+        previousStatement: "Case",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const BLOCKS = javascriptGenerator.statementToCode(block, 'BLOCKS');
+        const code = `default: ${BLOCKS}`;
+        return `${code}\n`;
+    })
+
+    // break
+    registerBlock(`${categoryPrefix}break`, {
+        message0: 'break',
+        args0: [],
+        previousStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const code = `break;`;
         return `${code}\n`;
     })
 }
