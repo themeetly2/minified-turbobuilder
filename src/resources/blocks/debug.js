959c9034b8b2939a67c5a1fd133845a879dc2312
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-08 15:49:57 +0000

    add more to debug category

--- src/resources/blocks/debug.js
+++ src/resources/blocks/debug.js
@@ -2,10 +2,10 @@ import javascriptGenerator from '../javascriptGenerator';
 import registerBlock from '../register';
 
 const categoryPrefix = 'debug_';
-const categoryColor = '#777777';
+const categoryColor = '#666666';
 
 function register() {
-    // if <> then {}
+    // console.log
     registerBlock(`${categoryPrefix}log`, {
         message0: 'log %1',
         args0: [
@@ -19,8 +19,46 @@ function register() {
         inputsInline: true,
         colour: categoryColor
     }, (block) => {
-        const LOG = block.getFieldValue('LOG')
+        const LOG = javascriptGenerator.valueToCode(block, 'LOG', javascriptGenerator.ORDER_ATOMIC);
         const code = `console.log(${LOG});`;
+        return `${code}\n`;
+    })
+
+    // console.warn
+    registerBlock(`${categoryPrefix}warn`, {
+        message0: 'warn %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "LOG"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const LOG = javascriptGenerator.valueToCode(block, 'LOG', javascriptGenerator.ORDER_ATOMIC);
+        const code = `console.warn(${LOG});`;
+        return `${code}\n`;
+    })
+
+    // console.error
+    registerBlock(`${categoryPrefix}error`, {
+        message0: 'error %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "LOG"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const LOG = javascriptGenerator.valueToCode(block, 'LOG', javascriptGenerator.ORDER_ATOMIC);
+        const code = `console.error(${LOG});`;
         return `${code}\n`;
     })
 }
