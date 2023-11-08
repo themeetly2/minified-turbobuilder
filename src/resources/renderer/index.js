c1662d7911cca764e8603f97404135ced09bd592
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-08 22:10:24 +0000

    this.stupid

--- src/resources/renderer/index.js
+++ src/resources/renderer/index.js
@@ -4,13 +4,13 @@ class CustomConstantProvider extends Blockly.blockRendering.ConstantProvider {
     constructor() {
         super();
 
-        SMALL_PADDING = 2
-        MEDIUM_PADDING = 4
-        MEDIUM_LARGE_PADDING = 6
-        LARGE_PADDING = 8
+        this.SMALL_PADDING = 2
+        this.MEDIUM_PADDING = 4
+        this.MEDIUM_LARGE_PADDING = 6
+        this.LARGE_PADDING = 8
 
-        NOTCH_WIDTH = 12
-        NOTCH_HEIGHT = 3
+        this.NOTCH_WIDTH = 12
+        this.NOTCH_HEIGHT = 3
     }
 }
 
@@ -18,7 +18,7 @@ export default class Renderer extends Blockly.zelos.Renderer {
     constructor() {
         super();
     }
-    
+
     makeConstants_() {
         return new CustomConstantProvider();
     }
