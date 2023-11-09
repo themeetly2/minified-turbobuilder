3c408f10d33b281bc83a790c2b835554aed31bfb
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-09 18:22:55 +0000

    funny little override

--- src/resources/register/index.js
+++ src/resources/register/index.js
@@ -2,6 +2,12 @@ import Blockly from 'blockly/core';
 import javascriptGenerator from '../javascriptGenerator';
 
 export default (blockName, jsonData, compileFunction) => {
+    for (const property in jsonData) {
+        if (['previousStatement', 'nextStatement'].includes(property) && jsonData[property] === null) {
+            jsonData[property] == 'Regular'
+        }
+    }
+
     const blockObject = {
         init: function () {
             this.jsonInit(jsonData);
