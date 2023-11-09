420805e6c80b7157083a06dd690edd625c2babd2
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-09 08:45:34 +0000

    more operators

--- src/resources/blocks/operators.js
+++ src/resources/blocks/operators.js
@@ -27,6 +27,79 @@ function register() {
 
         return [`${X || 0} == ${Y || 0}`, javascriptGenerator.ORDER_ATOMIC];
     })
+
+    // x === y
+    registerBlock(`${categoryPrefix}strictequals`, {
+        message0: '%1 === %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X"
+            },
+            {
+                "type": "input_value",
+                "name": "Y"
+            }
+        ],
+        output: "Boolean",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = javascriptGenerator.valueToCode(block, 'X', javascriptGenerator.ORDER_ATOMIC);
+        const Y = javascriptGenerator.valueToCode(block, 'Y', javascriptGenerator.ORDER_ATOMIC);
+
+        return [`${X || 0} === ${Y || 0}`, javascriptGenerator.ORDER_ATOMIC];
+    })
+
+    // x > y
+    registerBlock(`${categoryPrefix}more`, {
+        message0: '%1 > %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "Y",
+                "check": "Number"
+            }
+        ],
+        output: "Boolean",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = javascriptGenerator.valueToCode(block, 'X', javascriptGenerator.ORDER_ATOMIC);
+        const Y = javascriptGenerator.valueToCode(block, 'Y', javascriptGenerator.ORDER_ATOMIC);
+
+        return [`${X || 0} > ${Y || 0}`, javascriptGenerator.ORDER_ATOMIC];
+    })
+
+    // x > y
+    registerBlock(`${categoryPrefix}less`, {
+        message0: '%1 < %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "Y",
+                "check": "Number"
+            }
+        ],
+        output: "Boolean",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = javascriptGenerator.valueToCode(block, 'X', javascriptGenerator.ORDER_ATOMIC);
+        const Y = javascriptGenerator.valueToCode(block, 'Y', javascriptGenerator.ORDER_ATOMIC);
+
+        return [`${X || 0} < ${Y || 0}`, javascriptGenerator.ORDER_ATOMIC];
+    })
 }
 
 export default register;
