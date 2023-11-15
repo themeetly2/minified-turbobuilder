3646ca84ef8d1bd43bd66641a7dd5002b74d9932
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-15 22:24:53 +0000

    local storage blocks!

--- src/resources/blocks/variables.js
+++ src/resources/blocks/variables.js
@@ -26,7 +26,7 @@ function register() {
     }, (block) => {
         const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
         const VAR = javascriptGenerator.valueToCode(block, 'VAR', javascriptGenerator.ORDER_ATOMIC);
-        const code = `variables[${NAME}] = ${VAR || '""'}`;
+        const code = `variables[${NAME || '""'}] = ${VAR || '""'}`;
         return `${code}\n`;
     })
 
@@ -45,7 +45,49 @@ function register() {
         colour: categoryColor
     }, (block) => {
         const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
-        return [`variables[${NAME}]`, javascriptGenerator.ORDER_ATOMIC];
+        return [`variables[${NAME || '""'}]`, javascriptGenerator.ORDER_ATOMIC];
+    })
+    // set localStorage
+    registerBlock(`${categoryPrefix}setls`, {
+        message0: 'set localstorage %1 to %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "NAME",
+                "checks": "String"
+            },
+            {
+                "type": "input_value",
+                "name": "VAR"
+            }
+        ],
+        previousStatement: null,
+        nextStatement: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
+        const VAR = javascriptGenerator.valueToCode(block, 'VAR', javascriptGenerator.ORDER_ATOMIC);
+        const code = `localStorage.setItem(${NAME || '""'}, ${VAR || '""'})`;
+        return `${code}\n`;
+    })
+
+    // get localStorage
+    registerBlock(`${categoryPrefix}getls`, {
+        message0: 'get localstorage %1',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "NAME",
+                "checks": "String"
+            }
+        ],
+        output: null,
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const NAME = javascriptGenerator.valueToCode(block, 'NAME', javascriptGenerator.ORDER_ATOMIC);
+        return [`localStorage.getItem(${NAME || '""'})`, javascriptGenerator.ORDER_ATOMIC];
     })
 }
 
