90efc317c2b5e35a95657c052ae6f9e0cce7db46
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2023-11-18 08:07:01 +0000

    more json

--- src/resources/blocks/json.js
+++ src/resources/blocks/json.js
@@ -58,6 +58,53 @@ function register() {
         const INPUT = block.getFieldValue('INPUT')
         return [`JSON.stringify(${INPUT})`, javascriptGenerator.ORDER_ATOMIC];
     })
+
+    // insert
+    registerBlock(`${categoryPrefix}arrayinsert`, {
+        message0: 'insert %1 at end of %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X",
+            },
+            {
+                "type": "input_value",
+                "name": "Y",
+                "check": "JSONArray"
+            },
+        ],
+        output: "String",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = block.getFieldValue('X')
+        const Y = block.getFieldValue('Y')
+        return [`(${X}.push(${Y}))`, javascriptGenerator.ORDER_ATOMIC];
+    })
+
+    // get
+    registerBlock(`${categoryPrefix}arrayget`, {
+        message0: 'get %1 from array %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "Y",
+                "check": "JSONArray"
+            },
+        ],
+        output: "String",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = block.getFieldValue('X')
+        const Y = block.getFieldValue('Y')
+        return [`(${X}[${Y}])`, javascriptGenerator.ORDER_ATOMIC];
+    })
 }
 
 export default register;
