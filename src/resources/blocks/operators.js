2fa6242fa4c2175fb9f13599581f938d940e3818
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2024-07-28 14:40:28 +0100

    add modulo

--- src/resources/blocks/operators.js
+++ src/resources/blocks/operators.js
@@ -394,6 +394,30 @@ function register() {
 
         return [`(${X || 0} / ${Y || 1})`, javascriptGenerator.ORDER_ATOMIC];
     })
+    // x % y
+    registerBlock(`${categoryPrefix}modulo`, {
+        message0: '%1 modulo %2',
+        args0: [
+            {
+                "type": "input_value",
+                "name": "X",
+                "check": "Number"
+            },
+            {
+                "type": "input_value",
+                "name": "Y",
+                "check": "Number"
+            }
+        ],
+        output: "Number",
+        inputsInline: true,
+        colour: categoryColor
+    }, (block) => {
+        const X = javascriptGenerator.valueToCode(block, 'X', javascriptGenerator.ORDER_ATOMIC);
+        const Y = javascriptGenerator.valueToCode(block, 'Y', javascriptGenerator.ORDER_ATOMIC);
+
+        return [`(${X || 0} % ${Y || 1})`, javascriptGenerator.ORDER_ATOMIC];
+    })
 
     // x ^ y
     registerBlock(`${categoryPrefix}power`, {
